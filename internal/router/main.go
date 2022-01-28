10fdda13e8311abd4dd2b2f68a93f12a5a45bf29
mahdi-laptop <imani.mahdi@gmail.com>
2022-01-28 21:14:37 +0330

    wip: product metadata

--- internal/router/main.go
+++ internal/router/main.go
@@ -15,7 +15,7 @@ func RegisterRouter() *gin.Engine {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
